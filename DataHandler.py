eb07b1a82fcbdf45c2e33ec846b18c13cdea3444
yona <yonashay87@gmail.com>
2025-11-06 05:10:13 +0200

    import the data

--- DataHandler.py
+++ DataHandler.py
@@ -17,7 +17,8 @@ we have only one csv file in our data set so we will work directly at him as dat
 class ImportData:
     
     #import the data from kaggle to a directory
-    def ImportData(dir_path = "customers_data", data_path = "datasets/ulrikthygepedersen"):
+    @staticmethod
+    def import_data(dir_path = "customers_data", data_path = "datasets/ulrikthygepedersen"):
         path = pathlib.Path(dir_path)
         if path.exists():
             logging.warning("directory alreadt exist")
@@ -25,15 +26,20 @@ class ImportData:
         else:
             api = KaggleApi()
             api.authenticate()
-            api.dataset_download_files(path, data_path, unzip=True)
+            api.dataset_download_files(dataset=data_path, path=dir_path, unzip=True)
             logging.info("successfully imported the data to the directory")
 
     #transfer all the data csv file to data frame
+    @staticmethod
     def to_df(dir_path = "customers_data")->pd.DataFrame:
         data_path = pathlib.Path(dir_path)
         for file in data_path.iterdir:
-            df = pd.read_csv(file)
-            return df
-        
+            try:
+                df = pd.read_csv(file)
+                return df
+            except Exception as e:
+                logging.error(f"error has occured {e} while transfering the file")
+
+
 
 
